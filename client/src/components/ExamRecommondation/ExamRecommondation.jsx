863d3340aa71d846e55d8a6569079e567d70a0ff
Bhargav Jaldu <bhargavjaldu868860@gmail.com>
2022-03-14 15:44:17 +0530

    changes

---
+++ client/src/components/ExamRecommondation/ExamRecommondation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../NavBar/Nav'
+
+const ExamRecommondation = () => {
+    return (
+        <div>
+            <Nav />
+            <h1>Exam Recommondation</h1>
+        </div>
+    )
+}
+
+export default ExamRecommondation
